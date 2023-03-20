02ac7bc4b66fba6c4f8f6f745e0f61650fc4b172
pvanoostenbrugge <pvanoostenbrugge@gmail.com>
2023-03-21 00:16:58 +0100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using CodebaseAnalysisLib;
+using Microsoft.Maui.Controls;
+using System;
+using System.IO;
+
+namespace CodebaseAnalysisMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly CodebaseAnalyzer _analyzer;
+        private string _solutionPath;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _analyzer = new CodebaseAnalyzer();
+        }
+
+        // Add this method to handle the file picker functionality
+        private async void OnFilePickerClicked(object sender, EventArgs e)
+        {
+            var filePickerOptions = new PickOptions
+            {
+                FileTypes = new FilePickerFileType
+                    (new Dictionary<DevicePlatform, IEnumerable<string>>
+                        {{
+                        DevicePlatform.WinUI, new[] { ".sln" }
+                        },}
+                    )
+            };
+
+            var fileResult = await FilePicker.PickAsync(filePickerOptions);
+            if (fileResult != null)
+            {
+                _solutionPath = fileResult.FullPath;
+            }
+        }
+
+        private async void OnSubmitClicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(_solutionPath))
+            {
+                await DisplayAlert("Error", "Please select a .sln file.", "OK");
+                return;
+            }
+
+            var instruction = InputEditor.Text;
+
+            var codebaseInfo = await CodebaseAnalyzer.GetCodebaseInfo(_solutionPath, instruction);
+            var codeBasestring = codebaseInfo.ToString();
+
+            var codebaseInfoString = $"{codeBasestring}";
+            var ns = System.Reflection.Assembly.GetExecutingAssembly().EntryPoint.DeclaringType.Namespace;
+            string prompt = $"Given the following codebase information: \n\n Namespace: {ns} \n\n {codebaseInfoString} \n\n {instruction}";
+
+            CodebaseInfo.Text = prompt;
+        }
+    }
+}
