545395f0ece681eca87a4a8121bf3264aea0d484
pvanoostenbrugge <pvanoostenbrugge@gmail.com>
2023-03-21 19:50:34 +0100

    add dependencies en auto solution select

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,16 @@
 ﻿using CodebaseAnalysisLib;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Storage;
 using System;
 using System.IO;
+using System.Reflection;
 
 namespace CodebaseAnalysisMauiApp
 {
     public partial class MainPage : ContentPage
     {
+        
+
         private readonly CodebaseAnalyzer _analyzer;
         private string _solutionPath;
 
@@ -14,6 +18,8 @@ namespace CodebaseAnalysisMauiApp
         {
             InitializeComponent();
             _analyzer = new CodebaseAnalyzer();
+            _solutionPath = CodebaseAnalyzer.GetSolutionPath();
+            SolutionPathLabel.Text = $"Solution Path: {_solutionPath}";
         }
 
         // Add this method to handle the file picker functionality
@@ -33,17 +39,12 @@ namespace CodebaseAnalysisMauiApp
             if (fileResult != null)
             {
                 _solutionPath = fileResult.FullPath;
+                SolutionPathLabel.Text = $"Solution Path: {_solutionPath}";
             }
         }
 
         private async void OnSubmitClicked(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(_solutionPath))
-            {
-                await DisplayAlert("Error", "Please select a .sln file.", "OK");
-                return;
-            }
-
             var instruction = InputEditor.Text;
 
             var codebaseInfo = await CodebaseAnalyzer.GetCodebaseInfo(_solutionPath, instruction);
@@ -52,7 +53,7 @@ namespace CodebaseAnalysisMauiApp
             var codebaseInfoString = $"{codeBasestring}";
             var ns = System.Reflection.Assembly.GetExecutingAssembly().EntryPoint.DeclaringType.Namespace;
             string prompt = $"Given the following codebase information: \n\n Namespace: {ns} \n\n {codebaseInfoString} \n\n {instruction}";
-
+            prompt += $"\n\n output all parts 1 codeblock at a time.\r\nIf you cannot finish a codeblock before running out of characters/tokens, omit the codeblock and tell me to type continue";
             CodebaseInfo.Text = prompt;
         }
     }
